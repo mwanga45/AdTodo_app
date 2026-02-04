dc7d5fb7eb7dae9c1ff366286cc32340367d51d6
Your GitHub Username <your_email@example.com>
2026-02-04 15:52:51 +0300

    more changes

--- lib/core/services/local_storage.dart
+++ lib/core/services/local_storage.dart
@@ -3,7 +3,21 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 class LocalStorage {
 
   final FlutterSecureStorage _storage = const FlutterSecureStorage();
+  // _storage (underscore to make if  accessible  only in this  class) make it private
 
+  // Create an LOcalstorage  (Save the token)
+  Future<void> SaveToken(String token) async{
+    return  _storage.write(key: "auth_token", value: token);
+  }
+  // Access the token 
+  Future<String ?> getToken()async{
+    return _storage.read(key: "auth_token");
+  }
+
+  // Remove Token(Delete token)
+  Future<void>logout()async{
+    return _storage.delete(key: "auth_token");
+  }
 
 
 }
