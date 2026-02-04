54fdf698216533fd99e052304c7e8479b8651975
Your GitHub Username <your_email@example.com>
2026-02-04 16:59:54 +0300

    more changes

--- lib/core/services/app_wide.dart
+++ lib/core/services/app_wide.dart
@@ -1,13 +1,17 @@
 
+import 'package:dio/dio.dart';
 import 'package:todo_api/core/services/api_client.dart';
+import 'package:todo_api/src/modal/todo_info.dart';
 
 class TodoListService{
   final Apiclient  apiclient;
 
    TodoListService(this.apiclient);
 
-  Future<List<TodoInfo>> FetchTodolist() async{
-    await  apiclient.dio.get("/todoinfo");
-  }
+ Future <List<TodoInfo>> fetchtodoinfo()async{
+  final response = await apiclient.dio.get("/getlist");
+  return (response.data as List).map((e)=> TodoInfo.Json(e)).toList();
+
+ }
 
 }
