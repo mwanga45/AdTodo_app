90a05d15b776e8120a94e274d33dff5b9b30d061
Your GitHub Username <your_email@example.com>
2026-02-04 17:15:11 +0300

    more changes

--- lib/src/modal/todo_info.dart
+++ lib/src/modal/todo_info.dart
@@ -9,7 +9,7 @@ class TodoInfo {
     required this.title
   });
 
-  factory TodoInfo.Json(Map<String, dynamic>json){
+  factory TodoInfo.fromJson(Map<String, dynamic>json){
     return TodoInfo(completed: json['completed'], id: json['id'], title: json['title']);
   }
 
