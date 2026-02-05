ad4f99669284b3d22c08629a6fdc3030242ced89
Your GitHub Username <your_email@example.com>
2026-02-05 13:12:08 +0300

    more changes

---
+++ lib/src/controller/todo_controller.dart
@@ -0,0 +1,43 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:todo_api/core/services/app_wide.dart';
+import 'package:todo_api/src/modal/generic_response.dart';
+
+
+class TodoController extends ChangeNotifier {
+  final TodoListService todoListService;
+  // create constructor
+
+  TodoController(this.todoListService);
+  // create an  variable 
+  bool _loading = false;
+  String _message = "";
+  List<GenericResponse> _todo = [];
+
+  // Public getter (Read-only)
+  bool get loading => _loading;
+  String  get message => _message;
+  List<GenericResponse> get todo => _todo;
+
+  Future<void> createtodo()async{
+    _loading = true;
+    notifyListeners();
+    try {
+      final response =  await todoListService.createtodo();
+      if(response.success){
+        _message = response.message;
+
+      }
+      
+    } catch (e) {
+      _message = "Something went wrong";
+      
+    }finally{
+      _loading = false;
+      notifyListeners();
+    }
+  }
+
+
+}
