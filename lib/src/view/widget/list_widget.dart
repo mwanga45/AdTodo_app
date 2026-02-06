a9807e4c1e4fc1733597dae9bf65e33509884b47
mwanga45 <issamwanga02@gmail.com>
2026-02-06 10:33:45 +0300

    more change

--- lib/src/view/widget/list_widget.dart
+++ lib/src/view/widget/list_widget.dart
@@ -1,50 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
-// import 'package:todo_api/src/controller/todo_controller.dart';
-
-// class TodoListWidget extends StatelessWidget {
-//   const TodoListWidget({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Consumer<TodoController>(
-//       builder: (context, controller, _) {
-//         // 1️⃣ Loading state
-//         if (controller.loading) {
-//           return const Center(
-//             child: CircularProgressIndicator(),
-//           );
-//         }
-
-//         // 2️⃣ Message from backend
-//         if (controller.message != "") {
-//           return Text(
-//             controller.message!,
-//             style: const TextStyle(color: Colors.green),
-//           );
-//         }
-
-//         // 3️⃣ Empty state
-//         if (controller.todo.isEmpty) {
-//           return const Text("No todos yet");
-//         }
-
-//         // 4️⃣ Render todos
-//         return Expanded(
-//           child: ListView.builder(
-//             itemCount: controller.todo.length,
-//             itemBuilder: (context, index) {
-//               final todo = controller.todo[index];
-//               return ListTile(
-//                 title: Text(todo.title),
-//               );
-//             },
-//           ),
-//         );
-//       },
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_api/src/controller/todo_controller.dart';
@@ -64,7 +17,7 @@ class TodoList extends StatelessWidget {
         }
        if(controller.todo.isEmpty){
         return Center(
-          child: Text("No todo Available "),
+          child: Text("No todo Available ", style: TextStyle(fontSize: 30, color: Colors.black12, fontWeight: FontWeight.bold),),
         );
        }
       return ListView.builder(
